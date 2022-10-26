259490b405c5320b9965aeab4fe834ba73c3cf0b
giabao1511 <giabao712411@gmail.com>
2022-10-27 00:32:37 +0700

    up

---
+++ wp-admin/post.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://giabao1511.github.io/mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fproject%2Fwp-admin%2Fpost.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://giabao1511.github.io/mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fproject%2Fwp-admin%2Fpost.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://giabao1511.github.io/mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fproject%2Fwp-admin%2Fpost.php&reauth=1">https://giabao1511.github.io/mywordpress/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8888%2Fproject%2Fwp-admin%2Fpost.php&reauth=1</a></p>
+	</body>
+</html>
